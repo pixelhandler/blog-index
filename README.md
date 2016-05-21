fa50344371a0e72a5192dc3d4a3ad19534506a50
Bill Heaton <bill@mediaartslab.com>
2016-05-21 14:55:09 -0700

    Add README

---
+++ README.md
@@ -0,0 +1,6 @@
+# blog-index
+
+Sinatra app to serve index page for pixelhandler.com from Redis
+
+    sudo su - deploy
+    rackup -E production -p 4567 -s thin -D
--- app.rb
+++ app.rb
@@ -1,6 +1,5 @@
 require 'sinatra'
 require 'redis'
-require 'httparty'
 
 def bootstrap_index(index_key)
   redis = Redis.new(:password => "#{ENV['REDIS_SECRET']}")
@@ -9,16 +8,7 @@ def bootstrap_index(index_key)
   redis.get("#{project}:index:#{index_key}")
 end
 
-get '/sitemap.xml' do
-  content_type 'text/xml'
-  '<?xml version="1.0" encoding="UTF-8"?><url>sitemap.xml</url>'
-  #url = 'https://s3.amazonaws.com/cdn.pixelhandler.com/sitemap.xml'
-  #resp = HTTParty.get(url)
-  #resp.body
-end
-
 get '/*' do
   content_type 'text/html'
-  #"<!DOCTYPE html><html><head><title>Test</title></head><body><h1>Test</h1></body></html>"
   bootstrap_index(params[:index_key])
 end
